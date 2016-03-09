97a053e5ca8231624c6bc8b86172c0031cd53562
Will Schmid <william.schmid@weightwatchers.com>
2016-03-09 14:16:03 -0500

    remove $or and $ne

--- test/agenda.js
+++ test/agenda.js
@@ -900,7 +900,7 @@ describe("agenda", function() {
         setTimeout(function() {
           jobs.stop(function(err, res) {
             jobs._collection.findOne({name: 'longRunningJob'}, function(err, job) {
-              expect(job.lockedAt).to.be(null);
+              expect(job.lockedAt).to.eql(jobs._notLocked);
               done();
             });
           });
